f4c362a18c479042b5da12999bd646d0402be4cf
jhrvalente90 <jhrvalente90@github.com>
2021-02-02 23:15:01 +0000

    Commited all files to repository.

---
+++ build/precache-manifest.456c72eabc1636da2917312bc12edb87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d44fb15b7489a1e330712c1176cbe122",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab88e501f2912e23a67c",
+    "url": "/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "b4b1ca65dd3f1441e616",
+    "url": "/static/css/main.849bcc09.chunk.css"
+  },
+  {
+    "revision": "ab88e501f2912e23a67c",
+    "url": "/static/js/2.0637f928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0637f928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4b1ca65dd3f1441e616",
+    "url": "/static/js/main.062386b0.chunk.js"
+  },
+  {
+    "revision": "19cfff818a187e9cad09",
+    "url": "/static/js/runtime-main.d8ce210a.js"
+  }
+]);
